f7c9b6bf12367a8206ae39330617465aa5fe253a
gus <angusgyoung@gmail.com>
2019-10-30 19:30:30 +0000

    add basic spring project

---
+++ src/test/java/io/dotwave/isysserver/IsysServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dotwave.isysserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsysServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
