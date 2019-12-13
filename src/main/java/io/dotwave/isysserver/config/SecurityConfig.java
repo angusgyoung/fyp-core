646de7412c577ad7d6b70bf09fa8c1b62b72b98f
gus <angusgyoung@gmail.com>
2019-12-13 16:06:26 +0000

    fix decline cors error

--- src/main/java/io/dotwave/isysserver/config/SecurityConfig.java
+++ src/main/java/io/dotwave/isysserver/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers("/auth", "/profile/create").permitAll()
                 .anyRequest().authenticated().and()
