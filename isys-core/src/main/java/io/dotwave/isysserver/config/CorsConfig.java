24ad22ea0cf0cc8456ac8704d8ee4df0d85505bd
gus <angusgyoung@gmail.com>
2020-02-28 20:47:24 +0000

    add auth for socket connections

--- isys-core/src/main/java/io/dotwave/isysserver/config/CorsConfig.java
+++ isys-core/src/main/java/io/dotwave/isysserver/config/CorsConfig.java
@@ -5,11 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig extends WebMvcConfigurerAdapter {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
