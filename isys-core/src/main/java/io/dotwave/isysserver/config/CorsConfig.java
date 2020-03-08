7fb2a497ca98f0a877b2a2c29cb4ffe56f4c4e66
gus <angusgyoung@gmail.com>
2020-03-08 17:38:41 +0000

    add get allowed origins from environment

--- isys-core/src/main/java/io/dotwave/isysserver/config/CorsConfig.java
+++ isys-core/src/main/java/io/dotwave/isysserver/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package io.dotwave.isysserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,12 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${allowed-origin:*}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowCredentials(true);
             }
         };
     }
--- isys-core/src/main/java/io/dotwave/isysserver/config/MessageBrokerConfig.java
+++ isys-core/src/main/java/io/dotwave/isysserver/config/MessageBrokerConfig.java
@@ -1,5 +1,6 @@
 package io.dotwave.isysserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,11 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class MessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${allowed-origin:*}")
+    private String allowedOrigin;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // topics will be for things like posts (and maybe stuff to do with
-        // integrity checking, queues will be for things for specific users
-        // i.e a users posts
         config.enableSimpleBroker("/topic/", "/queue/");
         config.setApplicationDestinationPrefixes("/app");
     }
@@ -22,7 +23,7 @@ public class MessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/broker")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins(allowedOrigin)
                 .withSockJS();
     }
 
